5738907ea15d3236a9d968d8b16abe0aff3e49dc
algor1 <algor1@users.noreply.github.com>
2016-11-01 08:41:50 +0200

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -10,18 +10,18 @@ class PostsListView(ListView):
 class PostDetailView(DetailView):
     model = Task
 
-def task_new(request):
-        form = TaskForm()
-        return render(request, 'tasks/task_new.html', {'form': form})
+#def task_new(request):
+#        form = TaskForm()
+#        return render(request, 'tasks/task_new.html', {'form': form})
 
 def task_new(request):
         if request.method == "POST":
             form = TaskForm(request.POST)
             if form.is_valid():
-                post = form.save(commit=False)
-                post.author = request.user
-                post.published_date = timezone.now()
-                post.save()
+                task = form.save(commit=False)
+                task.author = request.user
+                task.published_date = timezone.now()
+                task.save()
                 return redirect('tasks.views.task_detail', pk=post.pk)
         else:
             form = TaskForm()
