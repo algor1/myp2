da634df8dd8cab7772466e817461689ae0588cd8
algor1 <algor1@users.noreply.github.com>
2016-11-01 08:39:56 +0200

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -14,3 +14,15 @@ def task_new(request):
         form = TaskForm()
         return render(request, 'tasks/task_new.html', {'form': form})
 
+def task_new(request):
+        if request.method == "POST":
+            form = TaskForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('tasks.views.task_detail', pk=post.pk)
+        else:
+            form = TaskForm()
+        return render(request,  'tasks/task_new.html', {'form': form})
