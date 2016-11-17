e093dc28a0d91ecec4219e7f238747ca4dcd04a5
algor1 <algor1@users.noreply.github.com>
2016-11-17 16:56:59 +0200

    Add files via upload

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Images
+from .models import Images, Task
 
 admin.site.register(Images)
+admin.site.register(Task)
